55a8e6c4e15cbece15c17caacbafeac894e75434
Daniel Bryant <daniel.bryant@tai-dev.co.uk>
2017-01-14 16:05:35 +0000

    Add missing Terraform config (WIP)

--- terraform/main.tf
+++ terraform/main.tf
@@ -24,17 +24,23 @@ POLICY
 }
 
 resource "aws_lambda_function" "hello_world_java" {
-    filename = "lambda_function_payload.zip"
+    filename = "${var.lambda_payload_filename}"
     function_name = "hello_world_java"
     role = "${aws_iam_role.iam_for_lambda.arn}"
-    handler = "exports.test"
-    source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
+    handler = "uk.co.danielbryant.exp.helloworld.Hello"
+    source_code_hash = "${base64sha256(file(var.lambda_payload_filename))}"
     runtime = "java8"
     environment {
         variables = {
             from = "London"
         }
     }
+}
+
+resource "aws_api_gateway_resource" "hello_world_api_gateway" {
+  rest_api_id = "${aws_api_gateway_rest_api.api.id}"
+  parent_id = "${aws_api_gateway_rest_api.api.root_resource_id}"
+  path_part = "mydemoresource"
 }
 
 resource "aws_api_gateway_rest_api" "api" {
@@ -44,7 +50,7 @@ resource "aws_api_gateway_rest_api" "api" {
 resource "aws_api_gateway_method" "method" {
   rest_api_id   = "${aws_api_gateway_rest_api.api.id}"
   resource_id   = "${aws_api_gateway_rest_api.api.root_resource_id}"
-  http_method   = "GET"
+  http_method   = "POST"
   authorization = "NONE"
 }
 
@@ -63,4 +69,14 @@ resource "aws_lambda_permission" "hello_world_java" {
   function_name = "${aws_lambda_function.hello_world_java.arn}"
   principal     = "apigateway.amazonaws.com"
   source_arn    = "arn:aws:execute-api:${var.region}:${var.account_id}:${aws_api_gateway_rest_api.api.id}/*/${aws_api_gateway_method.method.http_method}/"
+}
+
+resource "aws_api_gateway_deployment" "test_deploy" {
+  depends_on  = ["aws_api_gateway_integration.integration"]
+  stage_name  = "beta"
+  rest_api_id = "${aws_api_gateway_rest_api.api.id}"
+}
+
+output "path" {
+  value = "${aws_api_gateway_resource.hello_world_api_gateway.path}"
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -9,3 +9,7 @@ variable "region" {
 
 variable "account_id" {
 }
+
+variable "lambda_payload_filename" {
+  default = "../helloworldjava/target/helloworldjava-0.1.0-SNAPSHOT.jar"
+}
